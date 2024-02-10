a058042bb8b90c34acb409bec82a535ced898ed3
dandotcraig <dandotcraig@gmail.com>
2024-02-10 11:36:35 +1100

    create aefjioqeginerpijrgwpiwropigrwnioggrrw

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URI")
     app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
 
-
+    # connect libraries with flask app
     db.init_app(app)
     ma.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    from controllers.cli_controller import db_commands
+    app.register_blueprint(db_commands)
 
     return app
